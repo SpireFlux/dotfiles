4b4eaef72b06f9d89c11ef9a13c523c12ceefa04
Spireflux <spireflux@gmail.com>
2020-03-26 11:26:05 -0400

    updates to provision

--- provision_ubuntu.sh
+++ provision_ubuntu.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Run as root
 
+################################################################################
 # Update update and upgrade
+################################################################################
 apt-get update -y
-apt-get upgrade -y 
+apt-get upgrade -y
 
-# Install things with apt-get
-sudo apt-get install -y \
+################################################################################
+# Install with apt-get
+################################################################################
+apt-get install -y \
     build-essential \
     net-tools \
     vim \
@@ -17,8 +21,80 @@ sudo apt-get install -y \
     clang \
     cmake \
     nmap \
-    libc6-dev libc6-dev-i386 gcc-multilib g++-multilib 
+    libc6-dev libc6-dev-i386 gcc-multilib g++-multilib \
     apt-transport-https
+    python3-pip
+    ufw
+
+################################################################################
+# Gather apt repos
+# Sublime Text
+################################################################################
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+################################################################################
+# Nodejs
+################################################################################
+curl -sL https://deb.nodesource.com/setup_13.x | bash -
+# apt-get install -y nodejs
+
+################################################################################
+# Yarn
+################################################################################
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+################################################################################
+# Install remining items
+################################################################################
+apt-get update -y
+apt-get install -y \
+    sublime-text
+    nodejs
+    yarn
+
+################################################################################
+# Install Chrome
+################################################################################
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+
+################################################################################
+# Setup Tmux
+# install oh-my-tmux
+################################################################################
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+
+################################################################################
+# Configure tmux.conf.local
+################################################################################
+# Pull in tmux.conf.local
+
+################################################################################
+# Configure Git
+################################################################################
+# Pull in .gitconfig
+# Pull in .gitignore
+# Pull in .gitmessage
+
+
+################################################################################
+# Configure VIM
+################################################################################
+# Pull in .vimrc
+
+
+################################################################################
+# Configure bashrc
+################################################################################
+# Setup python and pip
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
 
 
 
@@ -29,59 +105,6 @@ sudo apt-get install -y \
 # sudo apt update
 # sudo apt install ubuntu-tweak
 
-
-# Using Ubuntu
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-
-# Sublime Text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-#echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#Dev
-echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#Update apt sources and install Sublime Text
-
-sudo apt-get update
-sudo apt-get install sublime-text
-
-
-# Setup Tmux Conf
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-# Setup git
-
-# Create a new ssh key?
-https://gitlab.com/help/ssh/README#generating-a-new-ssh-key-pair
-
-# Setup python and pip
-
-# Setup Go
-
-# Setup Web
-# Using Ubuntu
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-Add the Yarn APT repository to your system’s software repository list by typing:
-
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-Once the repository is added to the system, update the package list, and install Yarn, with:
-
-sudo apt update
-sudo apt install yarn
-
-
-
-
-
-
-# install chrome?
 
 
 
