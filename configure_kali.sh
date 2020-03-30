888fb380d8883cf2d97a3a34e47b4c320f896400
spireflux <spireflux@gmail.com>
2020-03-30 10:40:21 -0400

    updates to scripts

---
+++ configure_kali.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Run as user
+NAME='configure_kali.sh'
+
+# exit when any command fails
+set -e
+
+# install oh-my-tmux
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Clone oh-my-timux"
+echo -e "*******************************************************************\n"
+DIR="~/.tmux"
+if [ ! -d "$DIR" ]; then
+  	echo "tmux dir not exist"
+	# cd
+	# git clone https://github.com/gpakosz/.tmux.git
+	# ln -s -f .tmux/.tmux.conf
+	# cp .tmux/.tmux.conf.local .
+else
+	echo "${DIR} Already in place"
+fi
+
+
+# Clone dotfiles repo
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Clone dotfiles"
+echo -e "*******************************************************************\n"
+DIR="~/dotfiles"
+if [ ! -d "$DIR" ]; then
+  	echo "Cloning dotfiles"
+	cd
+	git clone https://github.com/SpireFlux/dotfiles .dotfiles
+else
+	echo "${DIR} Already in place"
+fi
+
+
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Configuring tmux.conf.local"
+echo -e "*******************************************************************\n"
+cp dotfiles/tmux.conf.local ~/.tmux.conf.local
+
+
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Configuring git setup"
+echo -e "*******************************************************************\n"
+cp dotfiles/gitconfig ~/.gitconfig
+cp dotfiles/gitignore_global ~/.gitignore_global
+cp dotfiles/gitmessage ~/.gitmessage
+
+
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Configuring vimrc"
+echo -e "*******************************************************************\n"
+cp dotfiles/vimrc ~/.vimrc
+
+
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Configuring bash_aliases"
+echo -e "*******************************************************************\n"
+cp dotfiles/bash_aliases ~/.bash_aliases
+
+
+echo -e "\n*******************************************************************"
+echo -e "${NAME} Configuring Sublime Preferences"
+echo -e "*******************************************************************\n"
+cp --remove-destination dotfiles/Preferences.sublime-settings  ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+
+
+################################################################################
+# source bashrc
+################################################################################
+source ~/.bashrc
+
+
+# Adjust terminal preferences, aka terminal font from cmd line script?
+
+# echo "10.10.10.151 sniper.htb" >> /etc/hosts
+# Setup python and pip
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
--- provision_kali.sh
+++ provision_kali.sh
@@ -34,7 +34,7 @@ apt-get install -qq \
     python3-pip \
     ufw
 
-# Gather apt repos
+
 echo -e "\n*******************************************************************"
 echo -e "${NAME} Add Sublime Repo"
 echo -e "*******************************************************************\n"
@@ -42,20 +42,17 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 
-# Nodejs
 echo -e "\n*******************************************************************"
 echo -e "${NAME} Add Nodejs repo "
 echo -e "*******************************************************************\n"
 curl -sL https://deb.nodesource.com/setup_13.x | bash -
 
 
-# Yarn
 echo -e "\n*******************************************************************"
 echo -e "${NAME} Add yarn repo"
 echo -e "*******************************************************************\n"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
 
 # Install remining items
 echo -e "\n*******************************************************************"
@@ -84,52 +81,3 @@ yarn --version
 echo "Sublime text:"
 subl --version
 
-
-# install oh-my-tmux
-# check if exisits?
-echo -e "\n*******************************************************************"
-echo -e "${NAME} Clone oh-my-timux"
-echo -e "*******************************************************************\n"
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-
-# check if exists?
-# Clone dotfiles repo
-echo -e "\n*******************************************************************"
-echo -e "${NAME} Clone dotfiles"
-echo -e "*******************************************************************\n"
-git clone https://github.com/SpireFlux/dotfiles
-
-
-################################################################################
-# Configure tmux.conf.local
-################################################################################
-#wget https://gist.githubusercontent.com/SpireFlux/1cd39bc9b8c99f9a2eaadf0413e94307/raw/d879d98bf5090775fb98bc78354494e3bae774b3/.tmux.conf.local
-
-
-################################################################################
-# Configure Git
-################################################################################
-# Pull in .gitconfig
-# Pull in .gitignore
-# Pull in .gitmessage
-
-
-################################################################################
-# Configure VIM
-################################################################################
-# Pull in .vimrc
-
-
-################################################################################
-# Configure bashrc
-################################################################################
-# Setup python and pip
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python get-pip.py
-
-
-# Adjust terminal preferences, aka terminal font from cmd line script?
